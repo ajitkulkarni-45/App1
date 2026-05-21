dc1fd5d324be0e6e52c4c23cf2e98dea6f70824c
ajit.kulkarni@gordian.com <ajit.kulkarni@gordian.com>
2026-05-21 19:15:58 +0530

    first commit

---
+++ WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebApplication1.Controllers
+{
+    //[ApiController]
+    [Route("api/products")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public ActionResult<ProductDto> Get(int id)
+        {
+            return Ok(new ProductDto
+            {
+                Id = id,
+                ProductName = "Keyboard",
+                Price = 1999
+            });
+        }
+    }
+}
