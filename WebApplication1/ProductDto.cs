e7bce6888fc63be22c8ea437973684d3bc8948f1
ajit.kulkarni@gordian.com <ajit.kulkarni@gordian.com>
2026-05-21 19:15:58 +0530

    first commit

---
+++ WebApplication1/ProductDto.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+
+        public string ProductName { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
